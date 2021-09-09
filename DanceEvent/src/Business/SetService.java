d85e5434c63c3d64b05091bd595ec584407b5c5e
Xuwei Hu <x.hu@arcady.nl>
2021-09-10 00:32:57 +0200

    Added business layer

---
+++ DanceEvent/src/Business/SetService.java
@@ -0,0 +1,32 @@
+package Business;
+
+import Entity.Set;
+
+import java.util.ArrayList;
+
+public class SetService {
+    public ArrayList<Set> getAllSets () {
+        // TODO: not implemented
+        return new ArrayList<Set>();
+    }
+
+    public Set getSet() {
+        return new Set(); // TODO: not implemented
+    }
+
+    public void reviewSet() {
+        // TODO: not implemented
+    }
+
+    public void updateSet() {
+        // TODO: not implemented
+    }
+
+    public void getSetReviews() {
+        // TODO: not implemented
+    }
+
+    public void addSet() {
+        // TODO: not implemented
+    }
+}
---
+++ DanceEvent/src/Console.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        boolean showMenu = true;
+        while (showMenu) {
+            int selection = mainMenu();
+
+            switch (selection) {
+                case 1:
+                    System.out.println("test item 1");
+                    break;
+                case 2:
+                    // Perform "encrypt number" case.
+                    break;
+                case 3:
+                    // Perform "decrypt number" case.
+                    break;
+                case 4:
+                    // Perform "quit" case.
+                    break;
+                case 5:
+                    // Perform "quit" case.
+                    break;
+                case 6:
+                    // Quit CLI
+                    showMenu = false;
+                    break;
+                default:
+
+            }
+        }
+    }
+
+    private static int mainMenu() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose action:");
+        System.out.println("1) - Show set info");
+        System.out.println("2) - Review set");
+        System.out.println("3) - Select and update set");
+        System.out.println("4) - Read set reviews");
+        System.out.println("5) - Add set");
+        System.out.println("6) - Quit");
+
+        return input.nextInt();
+
+    }
+}
